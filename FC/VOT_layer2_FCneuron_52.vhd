39bfe8667a3e545b9aab3555a3bf1053f17654db
RaulArceGonzalez <161450826+RaulArceGonzalez@users.noreply.github.com>
2024-06-19 11:06:27 +0200

    Add files via upload

---
+++ FC/VOT_layer2_FCneuron_52.vhd
@@ -0,0 +1,19 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.tfg_irene_package.ALL;
+use IEEE.NUMERIC_STD.ALL;
+use IEEE.MATH_REAL.ALL;
+
+entity VOT_layer2_FCneuron_52 is
+	Port ( 
+		 neuron_mac_1 : in std_logic_vector(input_size_L2fc+weight_size_L2fc + n_extra_bits-1 downto 0);
+		 neuron_mac_2 : in std_logic_vector(input_size_L2fc+weight_size_L2fc + n_extra_bits-1 downto 0);
+		 neuron_mac_3 : in std_logic_vector(input_size_L2fc+weight_size_L2fc + n_extra_bits-1 downto 0);
+		 neuron_mac_v : out std_logic_vector(input_size_L2fc+weight_size_L2fc + n_extra_bits-1 downto 0));
+end VOT_layer2_FCneuron_52 ;
+
+architecture Behavioral of VOT_layer2_FCneuron_52 is
+begin
+neuron_mac_v <= (neuron_mac_1 and neuron_mac_2) or (neuron_mac_1 and neuron_mac_3) or (neuron_mac_2 and neuron_mac_3);
+end Behavioral;
+
